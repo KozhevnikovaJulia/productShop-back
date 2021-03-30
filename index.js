190acfd29d644a995e3f73acb46970a1e36f12c5
KozhevnikovaJulia <Kozhevnikova1501@yandex.ru>
2021-03-30 22:07:13 +0300

    port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 7778;
+const port = process.env.PORT || 7778;
 const shopRouter = require('./shopRouter');
 const cartRouter = require('./cartRouter');
 const cors = require('cors');
